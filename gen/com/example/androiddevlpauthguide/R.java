2095b28445618d33d403715b1dfbe5eb053ef7d9
panjiaqiang <378290226@qq.com>
2013-04-25 18:04:37 +0800

    coustom menu of sixth

--- gen/com/example/androiddevlpauthguide/R.java
+++ gen/com/example/androiddevlpauthguide/R.java
@@ -16,20 +16,22 @@ public final class R {
         public static final int padding_small=0x7f040000;
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int image_left=0x7f020002;
-        public static final int image_right=0x7f020003;
-        public static final int star=0x7f020004;
+        public static final int file=0x7f020000;
+        public static final int headerfile=0x7f020001;
+        public static final int ic_action_search=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int image_left=0x7f020004;
+        public static final int image_right=0x7f020005;
+        public static final int star=0x7f020006;
     }
     public static final class id {
         public static final int activity_button_bt_five=0x7f080000;
         public static final int activity_checkedtext_listview_check=0x7f080001;
         public static final int activity_checkedtext_listview_multiple=0x7f080003;
         public static final int activity_checkedtext_listview_radio=0x7f080002;
-        public static final int activity_viewstub_bt_show=0x7f080004;
-        public static final int activity_viewstub_vs_one=0x7f080005;
-        public static final int menu_settings=0x7f080006;
+        public static final int activity_viewstub_bt_show=0x7f080005;
+        public static final int activity_viewstub_vs_one=0x7f080006;
+        public static final int main_button_addmenu=0x7f080004;
     }
     public static final class layout {
         public static final int activity_button=0x7f030000;
@@ -37,11 +39,13 @@ public final class R {
         public static final int activity_main=0x7f030002;
         public static final int activity_viewstub=0x7f030003;
         public static final int activity_viewstub_stub=0x7f030004;
+        public static final int custom_menu=0x7f030005;
     }
     public static final class menu {
-        public static final int activity_main=0x7f070000;
+        public static final int file_menu=0x7f070000;
     }
     public static final class string {
+        public static final int addmenu=0x7f05000d;
         public static final int app_name=0x7f050000;
         public static final int buttonfive=0x7f050008;
         public static final int buttonfour=0x7f050007;
--- src/com/example/androiddevlpauthguide/MainActivity.java
+++ src/com/example/androiddevlpauthguide/MainActivity.java
@@ -2,19 +2,109 @@ package com.example.androiddevlpauthguide;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.text.Layout;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
+import android.view.SubMenu;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.Button;
+import android.widget.PopupWindow;
 import android.support.v4.app.NavUtils;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity  {
+	private Button buttonAddMenu;
+	private Menu menu;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonAddMenu = (Button) this.findViewById(R.id.main_button_addmenu);
+        buttonAddMenu.setOnClickListener(new View.OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				if(menu != null){
+					for(int i = 10; i < 15; i++){
+						menu.add(1, i, i, "菜单" + i);
+					}
+				}
+				
+			}
+        	
+        });
+        registerForContextMenu(buttonAddMenu);
     }
-
-   
-
     
+   /*@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    this.menu = menu;
+		MenuItem fileitem = menu.add(1,1,1,"文件");
+		fileitem.setIcon(R.drawable.star);
+		fileitem.setIntent(new Intent(this,CheckedTextActivity.class));
+	   	MenuItem menuitemExit = menu.add(1,2,3,"退出");
+	   	//menuitemExit.setOnMenuItemClickListener(this);
+	   	menu.add(1,3,2,"更新");
+	   	SubMenu submenu = menu.addSubMenu(1, 4, 4, "子菜单");
+	   	submenu.setIcon(R.drawable.file);
+	   	submenu.setHeaderIcon(R.drawable.headerfile);
+	   	submenu.add(2, 5, 5, "新建");
+	   	submenu.add(2, 6, 6, "打开");
+	   	submenu.add(2, 7, 7, "退出");
+	   	submenu.setGroupCheckable(2, true, false);
+	   	getMenuInflater().inflate(R.menu.file_menu, menu);
+	   	return super.onCreateOptionsMenu(menu);
+	}*/
+
+	/*@Override
+	public boolean onMenuItemClick(MenuItem item) {
+		finish();
+		return false;
+	}
+   
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		super.onMenuItemSelected(featureId, item);
+		if(item.getItemId() == 2){
+			finish();
+		}
+		Log.i("data", "onMenuItemSelected");
+		return true;
+	}*/
+	
+	/*@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Log.i("data", "onOptionsItemSelected");
+		if(item.getItemId() == 2){
+			finish();
+		}
+		return super.onOptionsItemSelected(item);
+	}*/
+  /*  @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+    		ContextMenuInfo menuInfo) {
+    	super.onCreateContextMenu(menu, v, menuInfo);
+    	menu.setHeaderTitle("上下文菜单");
+    	menu.setHeaderIcon(R.drawable.file);
+    	menu.add(1, 1, 1, "菜单1");
+    }*/
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+    	if(keyCode == KeyEvent.KEYCODE_MENU){
+    		View layout = getLayoutInflater().inflate(R.layout.custom_menu, null);
+    		PopupWindow popwindow = new PopupWindow(layout,
+    						getWindowManager().getDefaultDisplay().getWidth(),
+    						getWindowManager().getDefaultDisplay().getHeight());
+    		popwindow.showAtLocation(layout,Gravity.BOTTOM , 0, 0);
+    		return true;
+    	}
+    	return super.onKeyDown(keyCode, event);
+    }
 }
